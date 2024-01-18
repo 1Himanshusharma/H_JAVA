7dbba7099a4cce12f08241e2ab5487f6580a4b7a
1Himanshusharma <himanshusharma272002@gmail.com>
2024-01-18 20:42:28 +0530

    jva

---
+++ basic/conditional.java
@@ -0,0 +1,12 @@
+class skydive{
+  public static void main(String[] args){
+    int age = 21;
+    if (age >= 18 && age > 0){
+      System.out.println("You can skydive.");
+    }
+    else{
+      System.out.println("Sorry, you are not old enough to skydive.");
+    }
+
+  }
+}
